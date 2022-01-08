10e6c612cab49c38dab952720ff3cb2ac950c558
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-01-08 16:20:52 +0530

    Update songdown.py

--- songdown.py
+++ songdown.py
@@ -101,6 +101,18 @@ def YT_info(yturl):
 
         return formats_available
 
+def humanbytes(num, suffix="B"):
+    if num is None:
+        num = 0
+    else:
+        num = int(num)
+
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, "Yi", suffix)
+
 def init_db():
     global db_mem
     db_mem = {}
