d8baa472cc86959469ac90219325f41ff5ca74e8
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-01-08 14:59:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 API_ID = int(os.getenv("API_ID", "8673250"))
 API_HASH = os.getenv("API_HASH", "bab0eb3130eb4930cc40112635b2dc4d")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+DURATION_LIMIT_MIN = int(os.getenv("DURATION_LIMIT", "60"))
